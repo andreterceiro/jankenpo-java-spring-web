a9504420710a45e3ae93367386b734a26d74f85f
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-19 22:29:57 -0300

    feat: removing methods that will not be used

--- src/main/java/br/com/terceiro/jankenpo/controllers/GameController.java
+++ src/main/java/br/com/terceiro/jankenpo/controllers/GameController.java
@@ -2,22 +2,15 @@ package br.com.terceiro.jankenpo.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.terceiro.jankenpo.models.Game;
 
-
 @Controller
 @RequestMapping("game")
 public class GameController {
-    @GetMapping("/play")
-    public ResponseEntity<String> play() {
-        return ResponseEntity.ok("Game started!");
-    }
-
     @PostMapping("/result")
     public ResponseEntity<String> result(@RequestBody String userOption) {
         try {
@@ -25,10 +18,5 @@ public class GameController {
         } catch (Exception e) {
             return ResponseEntity.status(422).body("Please select a valid option");
         }
-    }
-
-    @PostMapping("/test")
-    public ResponseEntity<String> test(@RequestBody String userOption) {
-        return ResponseEntity.ok("ok-".concat(userOption));
     }
 }
